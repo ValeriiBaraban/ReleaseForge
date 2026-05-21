ffc00b6f391e44708fa6b771479c1dbfe5074b41
Ars-R <82769848+Ars-R@users.noreply.github.com>
2026-05-21 16:05:16 -0700

    test app

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,8 @@
-// backend/server.js (или index.js)
 
-import sampleRoute from './routes/sample.js';
+import { sampleRoute, commitsRoute } from './routes/sample.js';
 import express, { json } from 'express';
 import { connect } from 'mongoose';
 import cors from 'cors';
-import commitsRoute from './routes/sample.js';
 import dotenv from 'dotenv';
 dotenv.config(); 
 const app = express();
@@ -16,25 +14,28 @@ app.use(cors({
 
 app.use(json()); 
 
+app.get('/1', (req, res) => {
+  res.send('!API is running');
+});
+
 
 const PORT = process.env.PORT || 8080;
 const MONGO_URI = process.env.MONGO_URI;
 
 connect(MONGO_URI)
-  .then(() => console.log('✅ Подключено к MongoDB Atlas'))
-  .catch((err) => console.error('❌ Ошибка подключения к БД:', err));
+  .then(() => console.log('connected to db'))
+  .catch((err) => console.error('error db:', err));
 
 
-// --- 3. МАРШРУТЫ (ROUTES) ---
-// ВОТ СЮДА МЫ ВСТАВЛЯЕМ НАШУ СТРОКУ:
+
 app.use('/api/sample', sampleRoute);
 app.use('/api/commits', commitsRoute);
-// (В будущем здесь появятся другие маршруты)
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/projects', require('./routes/projects'));
+app.use('/', (req, res) => {
+  res.send('API is running!');
+});
 
 
-// --- 4. ЗАПУСК СЕРВЕРА ---
+
 app.listen(PORT, () => {
   console.log(`🚀 Сервер запущен на порту ${PORT}`);
 });
