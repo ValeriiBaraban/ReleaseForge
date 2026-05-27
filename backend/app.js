f3c961f95bfce57b8556192adfb9564fc2b9451c
Ars-R <82769848+Ars-R@users.noreply.github.com>
2026-05-26 21:37:00 -0700

    fix(backend): correct passport env vars and start server

--- backend/app.js
+++ backend/app.js
@@ -61,8 +61,8 @@ passport.deserializeUser(async (id, done) => {
 });
 
 passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    clientID: process.env.GH_CLIENT_ID,
+    clientSecret: process.env.GH_CLIENT_SECRET,
     callbackURL: "https://projectsummer.click/api/auth/github/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
