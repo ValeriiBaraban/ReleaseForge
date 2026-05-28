6cd4af4de2d89b2704244f3575990041b9e5abff
Ars-R <82769848+Ars-R@users.noreply.github.com>
2026-05-28 12:09:04 -0700

    feat(backend): create encrypt/decrypt feathers for git token

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,50 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 import { use } from 'react';
+
+const algorithm = 'aes-256-cbc';
+
+const encrypt = (text) => {
+  if (!text) return text;
+    const secretKey = Buffer.from(process.env.ENCRYPTION_KEY, 'hex');
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return `${iv.toString('hex')}:${encrypted}`;
+};
+
+const decrypt = (text) => {
+  if (!text || !text.includes(':')) return text;
+  
+    const secretKey = Buffer.from(process.env.ENCRYPTION_KEY, 'hex');
+    const [ivHex, encryptedHex] = text.split(':');
+    const iv = Buffer.from(ivHex, 'hex');
+    const decipher = crypto.createDecipheriv(algorithm, secretKey, iv);
+    
+    let decrypted = decipher.update(encryptedHex, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    
+    return decrypted;
+};
 
 const userSchema = new mongoose.Schema({
   githubId: { type: String, required: true, unique: true, trim: true },
   username: { type: String, required: true, unique: true, trim: true },
   displayName: { type: String, trim: true },
   avatar: { type: String },
-  accessToken: { type: String, select: false },
-  email: { type: String, required: false, unique: true, sparse: true, lowercase: true, trim: true } 
-}, { timestamps: true });
+  accessToken: { 
+    type: String,
+    set: encrypt, 
+    get: decrypt  
+  },
+   email: { type: String, required: false, unique: true, sparse: true, lowercase: true, trim: true } 
+},
+  { timestamps: true },
+  { toJSON: { getters: true } },
+  { toObject: { getters: true } }
+);
 
 // userSchema.pre('save', async function (next) {
 //   try {
