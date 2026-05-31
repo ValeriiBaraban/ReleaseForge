40fc375eaee26ec260406f3d0862581952a4c187
Ars-R <82769848+Ars-R@users.noreply.github.com>
2026-05-31 16:32:46 -0700

    check projecttId

--- frontend/src/ProjectStats.jsx
+++ frontend/src/ProjectStats.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import './ProjectStats.css';
 
+console.log("Widget received projectId:", projectId);
+
 const ProjectStats = ({ projectId }) => {
   const [stats, setStats] = useState(null);
   const [error, setError] = useState(null);
