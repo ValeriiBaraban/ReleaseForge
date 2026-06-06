124287211983bfa17f4a8892a0902fe38b9855e9
Ars-R <82769848+Ars-R@users.noreply.github.com>
2026-06-05 22:01:12 -0700

    change cors

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 8080;
 app.set('trust proxy', true);
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'https://projectsummer.click',
   credentials: true 
 }));
 
