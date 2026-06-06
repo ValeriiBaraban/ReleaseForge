35e400b46bbb485f6af0b5e551a4948a037cfce4
Ars-R <82769848+Ars-R@users.noreply.github.com>
2026-06-05 18:31:11 -0700

    change cookie cred

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ app.use(session({
   }),
   cookie: {
     //TODO: return secure to true when deploying to production with HTTPS
-    secure: false, //process.env.NODE_ENV === 'production',
+    secure: true, //process.env.NODE_ENV === 'production',
     httpOnly: true, 
     sameSite: 'lax',
     maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
