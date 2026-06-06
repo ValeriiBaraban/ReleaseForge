10e6ac8fd75af5e4691e69076e79c92d369aeb9d
Ars-R <82769848+Ars-R@users.noreply.github.com>
2026-06-05 18:51:20 -0700

    add debug logs

--- backend/middlewares/authCheck.js
+++ backend/middlewares/authCheck.js
@@ -2,5 +2,9 @@ export const isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
+
+  console.log('Auth check failed! Request session:', req.sessionID);
+  console.log('User is authenticated:', req.isAuthenticated());
+  console.log('Request headers:', req.headers);
   res.status(401).json({ message: 'log in first' });
 };
